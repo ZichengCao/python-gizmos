0e3d700692ac230c5ccbb120f3d3fbc6462d8f9a
caozicheng <407182090@qq.com>
2024-02-27 16:12:17 +0800

    240227 密码管理器

--- 密码管理器/__init__.py
+++ 密码管理器/__init__.py
@@ -9,7 +9,38 @@ import string
 PASSWORD_FILE = "passwords.json"
 CONFIG_FILE = "config.json"
 
-from util import *
+def add_json_value(file_name, key, value):
+    # 读取现有的 JSON 数据
+    try:
+        with open(file_name, "r") as f:
+            existing_data = json.load(f)
+    except (FileNotFoundError, json.JSONDecodeError):
+        existing_data = {}
+
+    # 更新现有数据或创建新的数据
+    existing_data[key] = value
+
+    # 将更新后的数据写入文件
+    with open(file_name, "w") as f:
+        json.dump(existing_data, f)
+
+
+def add_json_array(file_name, key, json_data):
+    # 读取现有的 JSON 数据
+    try:
+        with open(file_name, "r") as f:
+            existing_data = json.load(f)
+    except (FileNotFoundError, json.JSONDecodeError):
+        existing_data = {}
+
+    # 更新现有数据或创建新的数据
+    if key not in existing_data:
+        existing_data[key] = []
+    existing_data[key].append(json_data)
+
+    # 将更新后的数据写入文件
+    with open(file_name, "w") as f:
+        json.dump(existing_data, f)
 
 
 # 加载密钥
@@ -53,7 +84,6 @@ def load_passwords(input_website):
                 passwords.append((username, decrypt(encrypted_password, load_key())))
 
     return passwords
-
 
 # 存储密码
 def store_password():
